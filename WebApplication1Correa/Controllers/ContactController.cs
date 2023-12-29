a5dcf91a46dc236d53be8755fd66d230fc14ccb4
CORREA BETSABE <betsabe.correa@tecnologicoliceocristiano.edu.ec>
2023-12-29 17:19:42 -0500

    Agregar archivos de proyecto.

---
+++ WebApplication1Correa/Controllers/ContactController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1Correa.Controllers
+{
+    public class ContactController : Controller
+    {
+        public string[] Get()
+        {
+            return new string[]
+            {
+             "Hello","World"
+            };
+
+        }
+
+    }
+}
+
+       
